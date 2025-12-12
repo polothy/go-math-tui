e56feb21d6ee3171ac8fe58c5f20ed01fc2cf675
Mark Nielsen <watermark.nielsen@gmail.com>
2025-12-11 18:01:29 -0800

    poc

--- go.mod
+++ go.mod
@@ -16,6 +16,9 @@ require (
 	github.com/charmbracelet/harmonica v0.2.0 // indirect
 	github.com/charmbracelet/x/exp/strings v0.0.0-20250911160549-0e720abcae8b // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/ebitengine/oto/v3 v3.4.0 // indirect
+	github.com/ebitengine/purego v0.9.0 // indirect
+	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
 	github.com/mitchellh/hashstructure/v2 v2.0.2 // indirect
 	github.com/muesli/clusters v0.0.0-20200529215643-2700303c1762 // indirect
 	github.com/muesli/kmeans v0.3.1 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bytes"
 	"errors"
 	"flag"
 	"fmt"
@@ -19,6 +20,8 @@ import (
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/huh"
 	"github.com/charmbracelet/lipgloss"
+	"github.com/ebitengine/oto/v3"
+	"github.com/hajimehoshi/go-mp3"
 	"github.com/lucasb-eyer/go-colorful"
 	"github.com/muesli/gamut"
 )
@@ -190,6 +193,8 @@ type model struct {
 	windowWidth  int
 	windowHeight int
 	splashWait   int
+
+	otoContext *oto.Context
 
 	// Stats
 	totalRight int
@@ -414,6 +419,55 @@ func (m model) View() string {
 }
 
 func main() {
+	// Read the mp3 file into memory
+	fileBytes, err := os.ReadFile("./sounds/level-up-enhancement-8-bit-retro-sound-effect-153002.mp3")
+	if err != nil {
+		panic("reading my-file.mp3 failed: " + err.Error())
+	}
+
+	// Convert the pure bytes into a reader object that can be used with the mp3 decoder
+	fileBytesReader := bytes.NewReader(fileBytes)
+
+	// Decode file
+	decodedMp3, err := mp3.NewDecoder(fileBytesReader)
+	if err != nil {
+		panic("mp3.NewDecoder failed: " + err.Error())
+	}
+
+	// Prepare an Oto context (this will use your default audio device) that will
+	// play all our sounds. Its configuration can't be changed later.
+
+	op := &oto.NewContextOptions{}
+
+	// Usually 44100 or 48000. Other values might cause distortions in Oto
+	op.SampleRate = 44100
+
+	// Number of channels (aka locations) to play sounds from. Either 1 or 2.
+	// 1 is mono sound, and 2 is stereo (most speakers are stereo).
+	op.ChannelCount = 2
+
+	// Format of the source. go-mp3's format is signed 16bit integers.
+	op.Format = oto.FormatSignedInt16LE
+
+	// Remember that you should **not** create more than one context
+	otoCtx, readyChan, err := oto.NewContext(op)
+	if err != nil {
+		panic("oto.NewContext failed: " + err.Error())
+	}
+	// It might take a bit for the hardware audio devices to be ready, so we wait on the channel.
+	<-readyChan
+
+	// Create a new 'player' that will handle our sound. Paused by default.
+	player := otoCtx.NewPlayer(decodedMp3)
+
+	// Play starts playing the sound and returns without waiting for it (Play() is async).
+	player.Play()
+
+	// We can wait for the sound to finish playing using something like this
+	for player.IsPlaying() {
+		time.Sleep(time.Millisecond)
+	}
+
 	m := parseFlags(initialModel())
 	if m.mode == modeNone {
 		m = runNewGameForm(m)
